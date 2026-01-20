f4b8d40fc78b57a42627211de9d923e0ab79588c
Shabad <shabadmk969@outlook.com>
2026-01-20 16:30:16 +0530

    cors middleware placement issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,6 +106,7 @@ MEDIA_URL = '/media/'
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -113,7 +114,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
